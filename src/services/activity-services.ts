37290a41f8ead23409ff71345771c9200c30485d
Adri Blanco <adrian-b.blanco@shell.com>
2023-01-29 22:50:18 +0100

    feat: added missing types

--- src/services/activity-services.ts
+++ src/services/activity-services.ts
@@ -1,9 +1,11 @@
+import { ActivityResponse } from "../types";
+
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function get() {
   const since = new Date("1-1-2023").getTime();
   return fetch(`${SERVER_URL}/activity?since=${since}`).then(async (res) => {
-    const jsonRes = await res.json();
+    const jsonRes: ActivityResponse = await res.json();
 
     return {
       distance: jsonRes.distance,
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,25 @@
 declare module "*.woff";
 declare module "*.ttf";
+
+export type Activity = {
+  avgSpeed: number;
+  elapsedTime: number;
+  maxSpeed: number;
+  avgCadence?: number;
+  distance: number;
+  athlete: number;
+  movingTime: number;
+  startDate: string;
+  wasRace: boolean;
+  name: string;
+  sportType: string;
+  elevationGain: number;
+  startDateUnix: number;
+};
+
+export type ActivityResponse = {
+  distance: number;
+  length: number;
+  since?: string;
+  activities: Activity[];
+};
