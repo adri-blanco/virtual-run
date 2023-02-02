bee74292213be8d35a4de8c954ccf4b168f5c12f
Adri Blanco <adrian-b.blanco@shell.com>
2023-02-03 00:07:43 +0100

    feat: added point and animations

--- src/components/Map/RouteMap.tsx
+++ src/components/Map/RouteMap.tsx
@@ -1,9 +1,14 @@
-import { MutableRefObject, useRef } from "react";
-import mapboxgl, { Map } from "mapbox-gl";
+import { MutableRefObject, useEffect, useRef } from "react";
+import mapboxgl, { GeoJSONSource, Map } from "mapbox-gl";
+import along from "@turf/along";
+import length from "@turf/length";
 import * as Styled from "./RouteMap.styles";
 import Route from "../../assets/camino-de-fisterra";
+import { useMapContext } from "../../context/map-context";
+import { Feature, Point, Position, lineString } from "@turf/helpers";
 
 const center = [-5.340904756902802, 43.20495172039625];
+const ANIMATION_STEP = 0.25;
 
 function addRoute(map: Map) {
   map.addSource("route", {
@@ -38,12 +43,77 @@ function centerView(map: Map) {
   });
 }
 
+function generatePoint(coord: Position): Feature<Point> {
+  return {
+    type: "Feature",
+    properties: {},
+    geometry: {
+      type: "Point",
+      coordinates: [coord[0], coord[1]],
+    },
+  };
+}
+
+function addPoint(map: Map) {
+  const coords = Route.features[0].geometry.coordinates;
+
+  map.addSource("athleteMarker", {
+    type: "geojson",
+    data: generatePoint(coords[0]),
+  });
+
+  map.addLayer({
+    id: "athleteIcon",
+    source: "athleteMarker",
+    type: "symbol",
+    layout: {
+      "icon-image": "pitch",
+      "icon-size": 1.5,
+      "icon-rotate": ["get", "bearing"],
+      "icon-rotation-alignment": "map",
+      "icon-allow-overlap": true,
+      "icon-ignore-placement": true,
+    },
+  });
+}
+
+function animate(map: Map, routeLength: number, distance: number, current = 0) {
+  const Coordinates = Route.features[0].geometry.coordinates;
+  if (routeLength <= current || distance <= current) {
+    return;
+  }
+
+  const nextPosition = along(
+    lineString(Coordinates),
+    current + ANIMATION_STEP,
+    { units: "kilometers" }
+  );
+
+  const source = map.getSource("athleteMarker") as GeoJSONSource;
+  source.setData(generatePoint(nextPosition.geometry.coordinates));
+
+  requestAnimationFrame(() =>
+    animate(map, routeLength, distance, current + ANIMATION_STEP)
+  );
+}
+
 function RouteMap() {
   const map = useRef<Map | null>(null);
+  const { distance, idle } = useMapContext();
+
+  useEffect(() => {
+    if (distance && idle && map.current) {
+      setTimeout(
+        () => animate(map.current as Map, length(Route), distance),
+        3500
+      );
+    }
+  }, [distance, idle]);
 
   function onLoad(mapRef: MutableRefObject<Map>) {
     map.current = mapRef.current;
     addRoute(map.current);
+    addPoint(map.current);
     centerView(map.current);
   }
 
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,8 +1,12 @@
 import * as Styled from "./Title.styles";
 import { useMapContext } from "../../context/map-context";
+import Route from "../../assets/camino-de-fisterra";
+import { useMemo } from "react";
+import length from "@turf/length";
 
 function Title() {
   const { idle, distance, activities } = useMapContext();
+  const destinationDistance = useMemo(() => length(Route), []);
 
   return (
     <Styled.Container small={idle}>
@@ -10,7 +14,9 @@ function Title() {
 
       {idle && distance && (
         <Styled.DataContainer>
-          <span>{distance?.toFixed(2)}/999km</span>
+          <span>
+            {distance?.toFixed(2)}/{destinationDistance.toFixed(2)}km
+          </span>
           <span>{activities} activities</span>
         </Styled.DataContainer>
       )}
